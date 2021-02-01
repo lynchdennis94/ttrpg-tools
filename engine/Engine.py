3d0691a3acdf3404a84360bc88fcb90ab0f92970
Dennis Lynch <lynch.dennis94@gmail.com>
2021-01-31 17:11:25 -0800

    Added logic to handle dice rolls and document how the dice functions work

--- engine/Engine.py
+++ engine/Engine.py
@@ -1,16 +1,34 @@
+import utilities.dice as Dice
+
+
 class Engine:
     def __init__(self):
         pass
 
     def run_engine(self):
+        """
+        Runs the ttrpg engine, providing an interactive session to call common commands
+
+        :return: None
+        """
         while True:
             # Get user input
-            user_input = input("command: ")
+            user_input = input("command: ").lower()
 
             # Execute action
             if user_input == 'quit':
                 break
             elif user_input == 'help':
                 print("TODO: Add Help")
+            elif user_input.startswith('roll'):
+                roll_input = user_input.split(' ')
+                if len(roll_input) != 2:
+                    print("Invalid command - run 'roll xdy' where\n"
+                          "x: the number of dice to roll\n"
+                          "y: the number of sides on a single die")
+                else:
+                    roll_values = Dice.parse_dice_notation(roll_input[1])
+                    if roll_values is not None:
+                        print(Dice.roll_die(int(roll_values[0]), int(roll_values[1])))
             else:
                 print("Unsupported command - try 'help'")
--- utilities/dice.py
+++ utilities/dice.py
@@ -1,5 +1,34 @@
 import random
 
 
-def roll_die(sides):
-    return random.randint(1, sides+1)
+def roll_die(number_of_dice, sides):
+    """
+    Rolls dice of the specified size, and returns a list of the roll outputs
+
+    :param number_of_dice: an int defining the number of dice to roll
+    :param sides: an int defining the number of sides on each die
+    :return: a list of ints, with each element representing the result of a single die roll
+    """
+    result = []
+    for roll in range(0, number_of_dice):
+        result.append(random.randint(1, sides))
+
+    return result
+
+
+def parse_dice_notation(dice_input):
+    """
+    Parses out the standard dice notation xdy, where x is the number of dice to roll, and y is the number of sides on
+    a single die
+
+    :param dice_input: a string of the format xdy
+    :return: a tuple (x,y) representing (number of dice, number of sides)
+    """
+    values = dice_input.split('d')
+    if len(values) != 2:
+        print("Invalid notation: please describe dice using xdy, where:\n"
+              "x: the number of dice to roll\n"
+              "y: the number of sides on each die")
+        return None
+    else:
+        return values[0], values[1]
